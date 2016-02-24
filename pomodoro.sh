3a069ce24583886f927643d625ceec27d7904798
Jeremy Seago <seagoj@gmail.com>
2016-02-24 16:20:53 -0600

    adds -d or --duration parameter to provide timer in minutes

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # POMODORO in YA TerMINAL ;)
 
-
 function pomo {
     RED='\033[0;31m'
     NC='\033[0m' # No Color
     ORANGE='\033[0;33m'
     printf "${ORANGE}POMODORO in YA TERMINAL${NC}\n"
-    
+
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "usage: pomo       25 minute cycle"
         echo -e "   or: pomo [break]['_message_']  see options below\n"
         echo "Options:"
+        echo "  d: timer duration in minutes"
         echo "  s: 05 minute break"
         echo "  l:  15 minute break"
         echo "  message: Your message to display"
         return
     fi
-    
+
     if [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
         echo -e "${ORANGE}POMODORO TIMER BY RUKY"
         echo "  v: 1"
@@ -26,47 +26,52 @@ function pomo {
         echo -e "  email: arkruka[@]gmail.com"
         return
     fi
-    
-    
+
     TITLE="PROMODO TIMER"
     ICON="face-cool"
     BEEP="_alarm 400 200"
-    
-    
-    if [ "$1" = "-l" ]; then
-        if [ -n "$2" ]; then
-            MESSAGE="$2"
-        else
+    TIMER=1500
+
+    while :
+    do
+        case "$1" in
+        -d | --duration)
+            TIMER=$(($2*60))
+            shift 2
+            ;;
+        -l | --long-break)
             MESSAGE="Long break over, back to work"
-        fi
-        
-        TIMER=900
-        echo -e "${RED}TIMER SET FOR 15 MINUTES"
-    
-    elif [ "$1" = "-s" ]; then
-        if [ -n "$2" ]; then
-            MESSAGE="$2"
-        else
+            TIMER=900
+            shift
+            ;;
+        -s | --short-break)
             MESSAGE="Short break over, back to work"
-        fi
-        
-        TIMER=300
-        echo -e "${RED}TIMER SET FOR 5 MINUTES"
-    
-    elif [ -n "$1" ]; then
+            TIMER=300
+            shift
+            ;;
+        -*)
+          echo "Error: Unknown option: $1" >&2
+          return 1
+          ;;
+        *)  # No more options
+          break
+          ;;
+        esac
+    done
+
+    if [ -n "$1" ]; then
         MESSAGE="$1"
-    
-    else
+    elif [ -z "$MESSAGE" ]; then
         MESSAGE="Time to take a break"
-        TIMER=1500
-        echo -e "${RED}TIMER SET FOR 25 MINUTES"
     fi
-    
+
+    echo -e "${RED}TIMER SET FOR ${$(($TIMER/60))} MINUTES"
+
     # LINUX users
-    if [ "$(uname)" == "Linux" ]; then
+    if [ "$(uname)" = "Linux" ]; then
         eval "(sleep $TIMER && notify-send '$TITLE' '$MESSAGE' --icon=$ICON && $BEEP &)"
     # MAC users
-    elif [ "$(uname)" == "Darwin" ]; then
+    elif [ "$(uname)" = "Darwin" ]; then
         eval "(sleep $TIMER && terminal-notifier -message '$MESSAGE' -title 'Pomodoro' --subtitle '$TITLE' &)"
     else
         echo "Sorry! Only Linux or Mac";
